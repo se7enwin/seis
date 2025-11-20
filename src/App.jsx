815a79eab486654d96fa6351fc680f8590e605d3
Ariel <74215813+se7enwin@users.noreply.github.com>
2025-11-20 15:24:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,9 @@ import { Route, Routes } from 'react-router-dom';
 export default function App() {
 
     return (
-<<<<<<< HEAD
         // Main Render Tag
         <div>
 
-<<<<<<< HEAD
             {/* Add first css attribute from jsx syntax */}
             <h3 style={{ color: 'green' }}>Function Component from Local Jsx </h3>
             <Nav />
@@ -17,11 +15,5 @@ export default function App() {
                 <Route path='/about' element={<About />} />
             </Routes>
         </div>
-=======
-=======
-        //Add first css attribute from jsx syntax
->>>>>>> fe66863 (Update index.js)
-        <h3 style={{ color: 'green' }}>Function Component from Local Jsx </h3>
->>>>>>> 1cd351a (Update App.Jsx)
     )
 }
