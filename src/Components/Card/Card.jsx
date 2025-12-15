3f08ac5db645d5b9e4686abbe6caebfba16293c4
Ariel <74215813+se7enwin@users.noreply.github.com>
2025-12-15 20:49:45 -0300

    Update Card.jsx

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,15 +3,21 @@ export default function Card({ name, image, house, wand, ancestry }) {
 
     return (
 
-        <h3>
-            <p>{name}</p>
-            <img src={image} />
-            <p>{house}</p>
-            <p display='inline'>{ancestry}</p>
-            <div> Magic Wand </div>  <hr />
-            <p>Madera: {wand.wood} <br />Centro: {wand.core} <br />Longitud: {wand.length}</p>
-
-        </h3>
+       < span > < p > {
+        name
+    } < /p><img src={image}/ > < div > {
+        house
+    } < /div><div display='inline'>{ancestry}</div >
+    <
+    hr / > < table border = '1'
+cellSpacing = "0" > < caption > MagicWand < /caption><thead><tr><th>Wood</th > < th > Core < /th><th>Length</th > < /tr></thead > < tbody > < tr >
+    <
+    td > {
+        wand.wood
+    } < /td><td>{wand.core}</td > < td > {
+        wand.length
+    } <
+    /td></tr > < /tbody></table > < /span>
 
 
 
