9b9c575806e5e44f5d3eaaae3fe38e1547e6bb99
se7enwin <se7enwin@hotmail.com>
2025-12-29 11:57:36 -0300

    update

--- src/Components/Detail/Detail.jsx
+++ src/Components/Detail/Detail.jsx
@@ -60,17 +60,3 @@ export default function Detail(props) {
 
 }
 
-// Borrar
-
-<span id='Card'>
-
-    <Link to={`/detail/${id}`}>
-        <p id="name">{name}</p>
-    </Link>
-    <img id='img' src={image} />
-    <div id='house'>{house}</div>
-    <div id='ancestry' display='inline'>{ancestry.charAt(0).toUpperCase() + ancestry.slice(1)}</div>
-    <hr />
-    <table id='magic' border='1' cellSpacing="0"><caption>MagicWand</caption><thead><tr><th>Wood</th><th>Core</th><th>Length</th> </tr></thead><tbody><tr><td>{wand.wood.charAt(0).toUpperCase() + wand.wood.slice(1)}</td><td>{wand.core.charAt(0).toUpperCase() + wand.core.slice(1)}</td><td>{wand.length}</td></tr></tbody></table>
-
-</span>
